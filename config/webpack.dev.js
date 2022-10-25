f00fad616a21683891867cc0d5ec6cb00b108977
pranay_kohad <prakohad@gmail.com>
2022-10-25 19:05:52 +0530

    prod deployment script added

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,7 +1,7 @@
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 const packageJson = require("../package.json");
 
 const devConfig = {
@@ -19,9 +19,6 @@ const devConfig = {
         marketing: "marketing@http://localhost:8081/remoteEntry.js",
       },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
